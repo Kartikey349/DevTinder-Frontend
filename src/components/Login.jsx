360661890014a5b956c1976f7a6655ba36dd6dd1
kartikey sharma <kartikey7518@gmail.com>
2025-08-02 03:25:34 +0530

    organised the files in component folder

---
+++ src/components/Login.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+
+const Login = () => {
+
+    const [emailId, setEmailId] = useState("kartikey@gmail.com");
+    const [password, setPassword] = useState("Sagar#123")
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleLogin = async () => {
+
+        const user = await axios.post(BASE_URL + "/login", {
+            emailId,
+            password
+        }, {
+            withCredentials: true
+        })
+
+        dispatch(addUser(user.data))
+        navigate("/");
+    }
+
+    return(
+        <div className="flex justify-center mt-20">
+            <div className="card card-border bg-base-300 w-96 flex">
+                <div className="card-body">
+                    <h2 className="card-title mx-auto">Login!</h2>
+
+                    <div>
+                        <fieldset className="fieldset">
+                            <legend className="fieldset-legend">Email</legend>
+                            <input type="text" className="input"
+                            value={emailId}
+                            onChange={(e) => setEmailId(e.target.value)}
+                            />
+                        </fieldset>
+                    </div>
+
+                    <div>
+                        <fieldset className="fieldset">
+                            <legend className="fieldset-legend">Password</legend>
+                            <input type="text" className="input"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </fieldset>
+                    </div>
+
+                    <div className="card-actions justify-center mt-2">
+                        <button className="btn btn-primary"
+                            onClick={handleLogin}
+                        >Submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
