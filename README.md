6e48523facce01ce65126ce5f04498e3e550c1bb
kartikey sharma <kartikey7518@gmail.com>
2025-08-02 02:26:31 +0530

    created login page and connected to the backend

---
+++ README.md
@@ -0,0 +1,20 @@
+# DevTinder Frontend
+
+
+- Create a vite + react app
+- install tailwind
+- install daisyUi
+- add navBar component to App.jsx
+- create a NavBar.jsx seprate component
+- install react-router-dom
+- Create BrowserRouter -> routes -> Route =/ Body -> RouteChildren
+- create outlet
+- create a footer
+- create a login page
+- install axios
+- CORS - install cors in backend => add midddle to App.js with configuration: origin, credentials: true
+- Whenver you're making api call so pass axios ==> {withCredentials: true}
+-
+-
+-
+-
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,53 @@
+import { useState } from "react";
+import axios from "axios";
+
 const Login = () => {
+
+    const [emailId, setEmailId] = useState("kartikey@gmail.com");
+    const [password, setPassword] = useState("Sagar#123")
+
+    const handleLogin = async () => {
+        await axios.post("http://localhost:7000/login", {
+            emailId,
+            password
+        }, {
+            withCredentials: true
+        })
+    }
+
     return(
-        <div>
-            login
+        <div className="flex justify-center mt-20">
+            <div className="card card-border bg-base-300 w-96 flex">
+                <div className="card-body">
+                    <h2 className="card-title mx-auto">Login!</h2>
+
+                    <div>
+                        <fieldset className="fieldset">
+                            <legend className="fieldset-legend">Email</legend>
+                            <input type="text" className="input"
+                            value={emailId}
+                            onChange={(e) => setEmailId(e.target.value)}
+                            />
+                        </fieldset>
+                    </div>
+
+                    <div>
+                        <fieldset className="fieldset">
+                            <legend className="fieldset-legend">Password</legend>
+                            <input type="text" className="input"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </fieldset>
+                    </div>
+
+                    <div className="card-actions justify-center mt-2">
+                        <button className="btn btn-primary"
+                            onClick={handleLogin}
+                        >Submit</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
