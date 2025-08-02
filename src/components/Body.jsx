22fe57530f74052814d2a8c849d3531ab6893890
kartikey sharma <kartikey7518@gmail.com>
2025-08-02 10:36:57 +0530

    check whether user loggedin or not, and show login page if not loggined

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,42 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import axios from "axios";
+import {BASE_URL} from "../utils/constants"
+import { useDispatch } from "react-redux";
+import {addUser} from "../utils/userSlice"
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Body = () => {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const user = useSelector((store) => store.user)
+
+    const fetchProfile = async () => {
+        try{
+            if(user) return;
+            const res = await axios.get(BASE_URL+ "/profile/view", {
+                withCredentials: true
+            })
+    
+            dispatch(addUser(res.data))
+        }catch(err){
+            if(err.status === 401){
+                navigate("/login")
+            }
+            console.log(err.message);
+        }
+    }
+
+    useEffect(() => {
+        fetchProfile();
+    }, [])
+
+
     return(
         <div>
             <NavBar />
